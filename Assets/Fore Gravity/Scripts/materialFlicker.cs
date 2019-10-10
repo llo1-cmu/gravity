cfe3955d70b48bf4b908d28eb2f147968218881c
Pipdoodle <llo1@andrew.cmu.edu>
2019-10-10 14:40:13 -0400

    startRoom

---
+++ Assets/Fore Gravity/Scripts/materialFlicker.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class materialFlicker : MonoBehaviour
+{
+    //private Material[] materials;
+    //[SerializeField] private MeshRenderer imageColor;
+    [SerializeField] private Material [] blueMaterial;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(fadeInAndOutRepeat(.5f, 4f));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+    IEnumerator fadeInAndOut(bool on)
+    {
+        
+            foreach (Material mat in blueMaterial)
+            {
+                Color colour = mat.GetColor("_EmissionColor");
+                if (on)
+                {
+                    colour *= 2.0f;
+                }
+                else
+                {
+                    colour /= 2.0f;
+                }
+                mat.SetColor("_EmissionColor", colour);
+         
+            }
+        yield return null;
+      
+    }
+
+    IEnumerator fadeInAndOutRepeat(float waitTime, float waitTime2)
+    {
+        float w1 = Random.Range(0f, waitTime);
+        float w2 = Random.Range(1f, waitTime2);
+        WaitForSeconds waitForXSec = new WaitForSeconds(w1);
+        WaitForSeconds waitForXSec2 = new WaitForSeconds(w2);
+
+        while (true)
+        {
+            //Fade out
+            yield return fadeInAndOut(false);
+
+            //Wait
+            yield return waitForXSec;
+
+            //Fade-in 
+            yield return fadeInAndOut(true);
+
+            yield return waitForXSec2;
+        }
+    }
+
+   
+}
